3287eb50301d2378c0843b30fced49f0b29210ba
diiegodm <diegoarevalodm@gmail.com>
2025-09-02 16:16:09 +0200

    soluciion del header

--- dist/portafolio/server/angular-app-manifest.mjs
+++ dist/portafolio/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6671, hash: '292845205e8fc6719b7ae2dc53bc6fcb6f69d4bd0fc6ef242dfb73c06d7a52d7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1115, hash: '0dd7b07384daabec6548f7cf38d3de699ea8b2a61e0ac413df23ffba304137d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 27959, hash: '5cff1fdf5ee6ac06e37397994c6a25e2ec7c54c74b64b70700c422143ceef9bc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-ABIBDZY3.css': {size: 20396, hash: 'btkItXdVOEc', text: () => import('./assets-chunks/styles-ABIBDZY3_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 6671, hash: '049b7c51a9877049f49d00105b8e85b1f88043490f72327280aad63e1328e59c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1115, hash: 'f4d7fc6c3e6e14edc391aa1b124e802ef611f2e714db976f7c509a8d4fbd7707', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 27863, hash: 'bf98f53d816abf09fc5da41e3d33d4f6e1a88fe65aa8932a5a1847e83b332a9b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-EEVFANBV.css': {size: 20587, hash: 'KJxHq16DNqs', text: () => import('./assets-chunks/styles-EEVFANBV_css.mjs').then(m => m.default)}
   },
 };
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+ menuOpen = false;
+
+  // Clases dinámicas de Tailwind para el menú
+ get menuClasses(): string {
+  return this.menuOpen
+    ? 'flex flex-col absolute top-16 left-0 w-full bg-black md:static md:flex-row md:w-auto transition-all duration-300'
+    : 'hidden flex-col md:flex md:flex-row md:static md:w-auto';
+}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
