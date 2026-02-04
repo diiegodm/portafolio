47604c468f7f8827531868e6e6d1acbce87c2bc8
diiegodm <diegoarevalodm@gmail.com>
2026-02-04 16:13:03 +0100

    reajustes

--- dist/portafolio/server/angular-app-manifest.mjs
+++ dist/portafolio/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6671, hash: '7765a8d511658fabebad95da95e314cca47c4bb059ba59002a4ea909853a8e00', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1115, hash: '98b10fdd4f9e102abaff814a366346eec407a6815e4cb68240c610028541da31', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 28062, hash: 'cbb555de21d846f8c60c5bb4338971262c0e98c63d541ffa437d7a5cc40e217e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-MH7GQADV.css': {size: 20333, hash: '5tk01bOYBVo', text: () => import('./assets-chunks/styles-MH7GQADV_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7518, hash: '06e495c61eb7653e077133a4696271a15b0e57f52eb8a201102bf3c021485fd8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1115, hash: 'ef814f910268464b2bec6088036082568a767e35596c16515ff08038e0842701', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 40499, hash: '1fbe758b00ead50ea8e24add1af8196b3ed45823f9d80bbbbd93e6011b24903b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-5DJZNETH.css': {size: 23298, hash: 'sxSfxxJIgvA', text: () => import('./assets-chunks/styles-5DJZNETH_css.mjs').then(m => m.default)}
   },
 };
